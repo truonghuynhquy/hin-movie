36ea26983a9b5cc1916d2b7f28864d5d5e712e54
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-09-28 23:03:52 +0700

    Create Folder and Index.vue

---
+++ resources/js/Pages/Casts/Index.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    <div>
+        <h1>Cast Index</h1>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ resources/js/Pages/Movies/Index.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    <div>
+        <h1>Movie Index</h1>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
