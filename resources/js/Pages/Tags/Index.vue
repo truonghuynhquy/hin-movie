131b3f06d526f6b6b24507624351ad3d8e4a1556
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-02 23:55:52 +0700

    Complete General Pagination

--- resources/js/Pages/Tags/Index.vue
+++ resources/js/Pages/Tags/Index.vue
@@ -105,27 +105,7 @@
                                 </tbody>
                             </table>
                             <div class="m-2 p-2">
-                                <div class="flex">
-                                    <template
-                                        v-for="(link, key) in tags.links"
-                                        :key="key"
-                                    >
-                                        <div
-                                            v-if="link.url === null"
-                                            v-html="link.label"
-                                            class="mb-1 mr-1 px-4 py-3 text-gray-400 text-sm leading-4 border border-gray-400 rounded"
-                                        ></div>
-                                        <Link
-                                            v-else
-                                            :href="link.url"
-                                            v-html="link.label"
-                                            class="mb-1 mr-1 px-4 py-3 text-gray-800 text-sm leading-4 border border-gray-800 rounded hover:text-indigo-700 hover:bg-gray-100"
-                                            :class="{
-                                                'bg-indigo-200': link.active,
-                                            }"
-                                        ></Link>
-                                    </template>
-                                </div>
+                                <Pagination :links="tags.links" />
                             </div>
                         </div>
                     </div>
@@ -134,13 +114,15 @@
         </div>
     </AdminLayout>
 </template>
+
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import { Link } from "@inertiajs/vue3";
-import { reactive } from "vue";
+import Pagination from "@/Components/Pagination.vue";
 
 const props = defineProps({
     tags: Object,
 });
 </script>
+
 <style lang=""></style>
