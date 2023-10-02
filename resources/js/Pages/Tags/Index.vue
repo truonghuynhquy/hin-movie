f922cd4dba6f332e4804e1b97c5bbfbd9c9ef23f
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-02 23:34:35 +0700

    Init paginate into UI

--- resources/js/Pages/Tags/Index.vue
+++ resources/js/Pages/Tags/Index.vue
@@ -104,7 +104,29 @@
                                     </tr>
                                 </tbody>
                             </table>
-                            <div class="m-2 p-2">links</div>
+                            <div class="m-2 p-2">
+                                <div class="flex">
+                                    <template
+                                        v-for="(link, key) in tags.links"
+                                        :key="key"
+                                    >
+                                        <div
+                                            v-if="link.url === null"
+                                            v-html="link.label"
+                                            class="mb-1 mr-1 px-4 py-3 text-gray-400 text-sm leading-4 border border-gray-400 rounded"
+                                        ></div>
+                                        <Link
+                                            v-else
+                                            :href="link.url"
+                                            v-html="link.label"
+                                            class="mb-1 mr-1 px-4 py-3 text-gray-800 text-sm leading-4 border border-gray-800 rounded hover:text-indigo-700 hover:bg-gray-100"
+                                            :class="{
+                                                'bg-indigo-200': link.active,
+                                            }"
+                                        ></Link>
+                                    </template>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
