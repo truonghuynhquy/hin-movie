f7401b05b57e9114070aeec3fb1d59ec4a8e4771
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-10 16:36:35 +0700

    Add  Cast Index And CastController to Show UI

--- app/Http/Controllers/Admin/CastController.php
+++ app/Http/Controllers/Admin/CastController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Cast;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class CastController extends Controller
 {
     function index()
     {
-        return Inertia::render('Casts/Index');
+        return Inertia::render('Casts/Index', [
+            'casts' => Cast::paginate(5),
+            'filters' => Request::only(['search', 'perPage']),
+        ]);
     }
 }
