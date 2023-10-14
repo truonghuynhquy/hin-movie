4df3a8e7e003d72c6b082d9dd30b13b25707b403
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-14 22:35:30 +0700

    Add Logout Function

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -241,6 +241,14 @@ const logout = () => {
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
                                     >Admin</a
                                 >
+                                <form @submit.prevent="logout">
+                                    <button
+                                        type="submit"
+                                        class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-indigo-600 hover:text-white focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                                    >
+                                        Log out
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
