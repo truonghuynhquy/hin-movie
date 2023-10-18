aae907c615db5e66f06554e23ec688baba2b94fd
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-18 17:42:14 +0700

    Generate Genres Index

--- app/Http/Controllers/Admin/GenreController.php
+++ app/Http/Controllers/Admin/GenreController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Genre;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class GenreController extends Controller
 {
-    function index()
+    public function index()
     {
-        return Inertia::render('Genres/Index');
+        $perPage = Request::input('perPage') ?: 5;
+
+        return Inertia::render('Genres/Index', [
+            'genres' => Genre::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('title', 'like', "%{$search}%");
+                })
+                ->paginate($perPage)
+                ->appends([
+                    'search' => Request::input('search'),
+                    'perPage' => Request::input('perPage')
+                ]),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 }
--- resources/js/Pages/Genres/Index.vue
+++ resources/js/Pages/Genres/Index.vue
@@ -1,9 +1,176 @@
 <template lang="">
-    <div>
-        <h1>Genre Index</h1>
-    </div>
+    <AdminLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Genres Index
+            </h2>
+        </template>
+
+        <div class="py-2">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <section class="container mx-auto p-6 font-mono">
+                    <div class="w-full flex mb-4 p-2 justify-end">
+                        <form
+                            class="flex space-x-4 shadow bg-white rounded-md m-2 p-2"
+                        >
+                            <div class="p-1">
+                                <button
+                                    type="button"
+                                    @click="generateGenre"
+                                    class="inline-flex items-center justify-center py-2 px-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out disabled:opacity-50"
+                                >
+                                    <span>Generate</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div
+                        class="w-full mb-8 overflow-hidden bg-white rounded-lg shadow-lg"
+                    >
+                        <div class="p-2 m-2">
+                            <div class="flex justify-between">
+                                <div class="flex-1">
+                                    <div class="relative">
+                                        <div
+                                            class="absolute flex items-center ml-2 h-full"
+                                        >
+                                            <svg
+                                                class="w-4 h-4 fill-current text-primary-gray-dark"
+                                                viewBox="0 0 16 16"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M15.8898 15.0493L11.8588 11.0182C11.7869 10.9463 11.6932 10.9088 11.5932 10.9088H11.2713C12.3431 9.74952 12.9994 8.20272 12.9994 6.49968C12.9994 2.90923 10.0901 0 6.49968 0C2.90923 0 0 2.90923 0 6.49968C0 10.0901 2.90923 12.9994 6.49968 12.9994C8.20272 12.9994 9.74952 12.3431 10.9088 11.2744V11.5932C10.9088 11.6932 10.9495 11.7869 11.0182 11.8588L15.0493 15.8898C15.1961 16.0367 15.4336 16.0367 15.5805 15.8898L15.8898 15.5805C16.0367 15.4336 16.0367 15.1961 15.8898 15.0493ZM6.49968 11.9994C3.45921 11.9994 0.999951 9.54016 0.999951 6.49968C0.999951 3.45921 3.45921 0.999951 6.49968 0.999951C9.54016 0.999951 11.9994 3.45921 11.9994 6.49968C11.9994 9.54016 9.54016 11.9994 6.49968 11.9994Z"
+                                                ></path>
+                                            </svg>
+                                        </div>
+
+                                        <input
+                                            v-model="search"
+                                            type="text"
+                                            placeholder="Search by title"
+                                            class="px-8 py-3 w-full md:w-2/6 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="flex">
+                                    <select
+                                        v-model="perPage"
+                                        @change="getCasts"
+                                        class="pl-4 pr-7 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                    >
+                                        <option value="null" disabled selected>
+                                            Choose Per Page
+                                        </option>
+                                        <option value="5">5 Per Page</option>
+                                        <option value="10">10 Per Page</option>
+                                        <option value="15">15 Per Page</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full overflow-x-auto">
+                            <Table>
+                                <template #tableHead>
+                                    <TableHead>Title</TableHead>
+                                    <TableHead>Slug</TableHead>
+                                    <TableHead>Manage</TableHead>
+                                </template>
+                                <TableRow
+                                    v-for="genre in genres.data"
+                                    :key="genre.id"
+                                >
+                                    <TableData>{{ genre.title }}</TableData>
+                                    <TableData>{{ genre.slug }}</TableData>
+                                    <TableData>
+                                        <div class="flex justify-around">
+                                            <ButtonLink
+                                                :link="
+                                                    route(
+                                                        'admin.genres.edit',
+                                                        genre.id
+                                                    )
+                                                "
+                                                >Edit</ButtonLink
+                                            >
+                                            <ButtonLink
+                                                class="bg-red-500 hover:bg-red-700"
+                                                :link="
+                                                    route(
+                                                        'admin.genres.destroy',
+                                                        genre.id
+                                                    )
+                                                "
+                                                method="delete"
+                                                as="button"
+                                                type="button"
+                                                >Delete</ButtonLink
+                                            >
+                                        </div>
+                                    </TableData>
+                                </TableRow>
+                            </Table>
+                            <div class="m-2 p-2">
+                                <Pagination :links="genres.links" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </AdminLayout>
 </template>
-<script>
-export default {};
+
+<script setup>
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import { Link } from "@inertiajs/vue3";
+import Pagination from "@/Components/Pagination.vue";
+import { ref, watch, defineProps } from "vue";
+import { router } from "@inertiajs/vue3";
+import Table from "@/Components/Table.vue";
+import TableHead from "@/Components/TableHead.vue";
+import TableRow from "@/Components/TableRow.vue";
+import TableData from "@/Components/TableData.vue";
+import ButtonLink from "@/Components/ButtonLink.vue";
+
+const props = defineProps({
+    genres: Object,
+    filters: Object,
+});
+
+const search = ref(props.filters.search);
+const perPage = ref(props.filters.perPage);
+// const showSpinner = ref(false);
+
+watch(search, (value) => {
+    router.get(
+        "/admin/genres",
+        { search: value, perPage: perPage.value },
+        { preserveState: true, replace: true }
+    );
+});
+
+function getGenres() {
+    router.get(
+        "/admin/genres",
+        { perPage: perPage.value, search: search.value },
+        { preserveState: true, replace: true }
+    );
+}
+
+function generateGenres() {
+    Inertia.post(
+        "/admin/genres"
+        // {},
+        // {
+        //     onStart: () => (showSpinner.value = true),
+        //     onFinish: () => (showSpinner.value = false),
+        // }
+    );
+}
 </script>
+
 <style lang=""></style>
