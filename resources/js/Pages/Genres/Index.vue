d84b98f628493b5020a08cb16c124a15dd67b0e3
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-21 20:00:40 +0700

    Add Spinner for Genre

--- resources/js/Pages/Genres/Index.vue
+++ resources/js/Pages/Genres/Index.vue
@@ -14,34 +14,36 @@
                             class="flex space-x-4 shadow bg-white rounded-md m-2 p-2"
                         >
                             <div class="p-1">
-                                <button
-                                    type="button"
-                                    @click="generateGenres"
-                                    class="inline-flex items-center justify-center py-2 px-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out disabled:opacity-50"
-                                >
-                                    <!-- <svg
-                                        v-if="showSpinner"
-                                        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
+                                <span class="inline-flex rounded-md shadow-sm">
+                                    <button
+                                        type="button"
+                                        @click="generateGenres"
+                                        class="inline-flex items-center justify-center py-2 px-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out disabled:opacity-50"
                                     >
-                                        <circle
-                                            class="opacity-25"
-                                            cx="12"
-                                            cy="12"
-                                            r="10"
-                                            stroke="currentColor"
-                                            stroke-width="4"
-                                        ></circle>
-                                        <path
-                                            class="opacity-75"
-                                            fill="currentColor"
-                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                        ></path>
-                                    </svg> -->
-                                    <span>Generate</span>
-                                </button>
+                                        <svg
+                                            v-if="showSpinner"
+                                            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <circle
+                                                class="opacity-25"
+                                                cx="12"
+                                                cy="12"
+                                                r="10"
+                                                stroke="currentColor"
+                                                stroke-width="4"
+                                            ></circle>
+                                            <path
+                                                class="opacity-75"
+                                                fill="currentColor"
+                                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                            ></path>
+                                        </svg>
+                                        Generate
+                                    </button>
+                                </span>
                             </div>
                         </form>
                     </div>
@@ -164,7 +166,7 @@ const props = defineProps({
 
 const search = ref(props.filters.search);
 const perPage = ref(props.filters.perPage);
-// const showSpinner = ref(false);
+const showSpinner = ref(false);
 
 watch(search, (value) => {
     router.get(
@@ -184,12 +186,12 @@ function getGenres() {
 
 function generateGenres() {
     router.post(
-        "/admin/genres"
-        // {},
-        // {
-        //     onStart: () => (showSpinner.value = true),
-        //     onFinish: () => (showSpinner.value = false),
-        // }
+        "/admin/genres",
+        {},
+        {
+            onStart: () => (showSpinner.value = true),
+            onFinish: () => (showSpinner.value = false),
+        }
     );
 }
 </script>
