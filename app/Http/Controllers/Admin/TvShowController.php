7aea0db9e6d1d90989d9b5399704712f6c5a776f
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-10-24 19:16:35 +0700

    UI Admin of TV Shows

--- app/Http/Controllers/Admin/TvShowController.php
+++ app/Http/Controllers/Admin/TvShowController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\TvShow;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 
 class TvShowController extends Controller
 {
-    function index()
+    public function index()
     {
-        return Inertia::render('TvShows/Index');
+        $perPage = Request::input('perPage') ?: 5;
+
+        return Inertia::render('TvShows/Index', [
+            'tvShows' => TvShow::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate($perPage)
+                ->appends([
+                    'search' => Request::input('search'),
+                    'perPage' => Request::input('perPage')
+                ]),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 }
