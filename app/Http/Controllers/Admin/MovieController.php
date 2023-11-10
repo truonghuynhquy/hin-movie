f049429abfe5dab3b9b3bd49cd8b9bfcaac26e6e
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-11-10 20:28:48 +0700

    Add movie search and pagination functionality to Movies/Index view.

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Genre;
+use App\Models\Movie;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class MovieController extends Controller
 {
     function index()
     {
-        return Inertia::render('Movies/Index');
+        $perPage = Request::input('per_page') ?: 5;
+        return Inertia::render('Movies/Index', [
+            'movies' => Movie::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('title', 'LIKE', "%{$search}%");
+                })
+                ->paginate($perPage)
+                ->appends([
+                    'search' => Request::input('search'),
+                    'perPage' => Request::input('perPage')
+                ]),
+            'filters' => Request::only(['search', 'perPage']),
+        ]);
     }
 }
