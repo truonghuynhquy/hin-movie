a4152d834df24776f8b08f97f169358364a63593
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-12-26 10:56:09 +0700

    Update movie show page layout and add modal for successful payment

--- resources/js/Pages/Frontend/Movies/Show.vue
+++ resources/js/Pages/Frontend/Movies/Show.vue
@@ -1,6 +1,7 @@
 <template lang="">
+    <Head :title="`${movie.title}`" />
     <FrontLayout>
-        <main v-if="movie" class="my-2">
+        <main v-if="movie" class="mt-[-8px]">
             <section class="bg-gradient-to-r from-indigo-800 to-transparent">
                 <div class="max-w-6xl mx-auto m-4 p-2">
                     <div class="flex">
@@ -74,7 +75,7 @@
                                     </div>
 
                                     <button
-                                        class="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                                        class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                                         v-for="trailer in trailers"
                                         :key="trailer.id"
                                         @click="openModal(trailer)"
@@ -162,6 +163,66 @@
             </section>
         </main>
     </FrontLayout>
+
+    <TransitionRoot appear :show="isOpen" as="template">
+        <Dialog as="div" @close="closeModal" class="relative z-10">
+            <TransitionChild
+                as="template"
+                enter="duration-300 ease-out"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="duration-200 ease-in"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+            >
+                <div class="fixed inset-0 bg-black/25" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto">
+                <div
+                    class="flex min-h-full items-center justify-center p-4 text-center"
+                >
+                    <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95"
+                    >
+                        <DialogPanel
+                            class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                        >
+                            <DialogTitle
+                                as="h3"
+                                class="text-lg font-medium leading-6 text-gray-900"
+                            >
+                                Payment successful
+                            </DialogTitle>
+                            <div class="mt-2">
+                                <p class="text-sm text-gray-500">
+                                    Your payment has been successfully
+                                    submitted. We’ve sent you an email with all
+                                    of the details of your order.
+                                </p>
+                            </div>
+
+                            <div class="mt-4">
+                                <button
+                                    type="button"
+                                    class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                    @click="closeModal"
+                                >
+                                    Got it, thanks!
+                                </button>
+                            </div>
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
 </template>
 <script setup>
 import { ref } from "vue";
@@ -190,6 +251,18 @@ defineProps({
 const components = {
     CircleProgress,
 };
+
+const isOpen = ref(false);
+const modalTrailer = ref({});
+
+function closeModal() {
+    isOpen.value = false;
+}
+
+function openModal(trailer) {
+    modalTrailer.value = trailer;
+    isOpen.value = true;
+}
 </script>
 <style>
 .circle_progressbar.vue3-circular-progressbar svg {
