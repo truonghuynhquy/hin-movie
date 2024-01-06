27d338fc5fd6478c28d963f1c43c37348a4e212f
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2024-01-06 16:24:31 +0700

    Assign 'admin' role to authenticated user in dashboard route

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,6 @@ Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->prefix('admin')->
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    // auth()->user()->assignRole('admin');
+    auth()->user()->assignRole('admin');
     return Inertia::render('Dashboard');
 })->name('dashboard');
